4ea8dbe434a6f1985ffc9af12400999bd8384b95
ginger <26461046+gingerchew@users.noreply.github.com>
2024-12-05 16:41:22 -0500

    Adds initial implementation

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,41 @@
+
 // Example use for the demo plugin:
 // {{ 'Steph' | hello | safe }}
+import { transform } from "@astrojs/compiler";
+import { writeFile, mkdir } from 'node:fs/promises';
 
-module.exports = (eleventyConfig, options) => {
-  // Define defaults for your plugin config
-  const defaults = {
-    htmlTag: "h2",
-  };
+/**
+ * Currently creates the folder to store the typescript modules
+ * Need to find a way to parse them to JS and import and run to return the necessary html
+ * 
+ * Need to learn more about how the node file stuff works
+ * for whatever reason it's saying the file exists but I can't find it
+ * it looks like the typescript code is legit
+ */
 
-  // You can create more than filters as a plugin, but here's an example
-  eleventyConfig.addFilter("hello", (name) => {
-    // Combine defaults with user defined options
-    const { htmlTag } = {
-      ...defaults,
-      ...options,
-    };
+export default (eleventyConfig, options) => {
+  eleventyConfig.addTemplateFormats("astro")
+  const hash = 'q7098aksjfhao'// arbitrary string
+  eleventyConfig.addExtension('astro', {
+    compile: async (str, inputPath) => {
+      const folder = '.astro'+hash
+      
+      await mkdir(folder)
 
-    return `<${htmlTag}>Hello, ${name}!</${htmlTag}>`;
-  });
+      // create a ts module that returns html
+      const result = await transform(str, {
+        filename: inputPath,
+        internalURL: "astro/runtime/server/index.js",
+      })
+
+      
+      const filePath = folder+inputPath.slice(1).split('/').at(-1)
+      
+      await writeFile(filePath.replace('.astro','.ts'), result.code)
+
+      return async function() {
+        const mod = await import('./'+filePath)
+      }
+    }
+  })
 };
--- index.md
+++ index.md
@@ -1,5 +1,5 @@
 ---
-layout: base.njk
+layout: base.astro
 ---
 
 ## Plugin Test Page
