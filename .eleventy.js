95f73fb63e32a161b699b5add223457eb930392e
ginger <26461046+gingerchew@users.noreply.github.com>
2024-12-07 18:02:29 -0500

    Adds container api and compiler
    Transforms the .astro files into a typescript module still
    but fails to find the astro/runtime/.. stuff
    
    going to ask around to see if someone understands that better

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 
 // Example use for the demo plugin:
 // {{ 'Steph' | hello | safe }}
-import { transform } from "@astrojs/compiler";
-import { writeFile, mkdir } from 'node:fs/promises';
+import { transform, compile } from "@astrojs/compiler";
+import { experimental_AstroContainer } from "astro/container";
+import mdxRenderer from '@astrojs/mdx/server.js'
+// import { writeFile, mkdir } from 'node:fs/promises';
 
 /**
  * Currently creates the folder to store the typescript modules
@@ -12,29 +14,28 @@ import { writeFile, mkdir } from 'node:fs/promises';
  * for whatever reason it's saying the file exists but I can't find it
  * it looks like the typescript code is legit
  */
+const container = await experimental_AstroContainer.create()
+container.addServerRenderer({ renderer: mdxRenderer })
 
 export default (eleventyConfig, options) => {
   eleventyConfig.addTemplateFormats("astro")
-  const hash = 'q7098aksjfhao'// arbitrary string
+  
   eleventyConfig.addExtension('astro', {
     compile: async (str, inputPath) => {
-      const folder = '.astro'+hash
       
-      await mkdir(folder)
-
       // create a ts module that returns html
-      const result = await transform(str, {
-        filename: inputPath,
-        internalURL: "astro/runtime/server/index.js",
+      const transformed = await transform(str, {
+        filename: inputPath
       })
+      const module = await compile(transformed.code)
+      console.log(module)
+      // const parsed = await parse(Mod.code)
+      // console.log(module)
 
+      // const html = await container.renderToString(Mod.code);
       
-      const filePath = folder+inputPath.slice(1).split('/').at(-1)
-      
-      await writeFile(filePath.replace('.astro','.ts'), result.code)
-
       return async function() {
-        const mod = await import('./'+filePath)
+        return ' '
       }
     }
   })
